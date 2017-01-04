b23c26234c8b5a9c331cfe5646ac7110cb040457
Evan Porter <evan.porter@dotman.ca>
2017-01-04 18:32:58 -0400

    Refactored catalog to field

--- pos.py
+++ pos.py
@@ -1,16 +1,16 @@
 class PointOfSale():
     def __init__(self, display):
         self.display = display
+        self.catalog = {"12345": "$9.50", "98765": "$1.50"}
 
     def on_barcode(self, barcode):
         if not barcode:
             self.display.display_text("The barcode is invalid")
             return
 
-        catalog = {"12345": "$9.50", "98765": "$1.50"}
 
-        if barcode in catalog.keys():
-            self.display.display_text(catalog[barcode])
+        if barcode in self.catalog.keys():
+            self.display.display_text(self.catalog[barcode])
         else:
             self.display.display_text("Item with barcode {} not found".format(barcode))
 
