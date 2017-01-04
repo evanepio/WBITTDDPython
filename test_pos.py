a817afc55d8edac9fc5a3058df0c188817e0070e
Evan Porter <evan.porter@dotman.ca>
2017-01-03 21:31:18 -0400

    Added second test

--- test_pos.py
+++ test_pos.py
@@ -1,16 +1,36 @@
 class PointOfSale():
-    def on_barcode(selfself, barcode):
-        pass
+    def __init__(self, display):
+        self.display = display
+
+    def on_barcode(self, barcode):
+        if barcode == "12345":
+            self.display.display_text("$9.50")
+        elif barcode == "98765":
+            self.display.display_text("$1.50")
+
 
 class Display:
     def get_text(self):
-        return "$9.50"
+        return self.__text
+
+    def display_text(self, value):
+        self.__text = value
+
 
 def test_item_found():
-    sut = PointOfSale()
     display = Display()
+    sut = PointOfSale(display)
 
     # Test
-    sut.on_barcode('12345')
+    sut.on_barcode("12345")
 
     assert display.get_text() == "$9.50"
+
+
+def test_another_item_found():
+    display = Display()
+    sut = PointOfSale(display)
+
+    sut.on_barcode("98765")
+
+    assert display.get_text() == "$1.50"
