c71516fc06bdbf7856af08e989e8de70f6d55087
Evan Porter <evan.porter@dotman.ca>
2017-01-07 13:35:13 -0400

    display_invalid_barcode_message extracted

--- pos.py
+++ pos.py
@@ -5,13 +5,16 @@ class PointOfSale():
 
     def on_barcode(self, barcode):
         if not barcode:
-            self.display_message("The barcode is invalid")
+            self.display_invalid_barcode_message()
             return
 
         if barcode in self.catalog.keys():
             self.display_message(self.catalog[barcode])
         else:
             self.display_message("Item with barcode {} not found".format(barcode))
+
+    def display_invalid_barcode_message(self):
+        self.display_message("The barcode is invalid")
 
     def display_message(self, message):
         self.display.display_text(message)
