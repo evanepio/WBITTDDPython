861dadc256f8062c7ac07deee0ba74124d3f5968
Evan Porter <evan.porter@dotman.ca>
2017-01-07 14:45:12 -0400

    Fixed the inconsistent price type, which and removed float call. Me dumb.

--- test_pos.py
+++ test_pos.py
@@ -12,7 +12,7 @@ def test_item_found():
 
 def test_another_item_found():
     display = Display()
-    sut = PointOfSale(display, {"12345": "9.50", "98765": "1.50"})
+    sut = PointOfSale(display, {"12345": 9.50, "98765": 1.50})
 
     sut.on_barcode("98765")
 
@@ -21,7 +21,7 @@ def test_another_item_found():
 
 def test_item_not_found():
     display = Display()
-    sut = PointOfSale(display, {"12345": "9.50"})
+    sut = PointOfSale(display, {"12345": 9.50})
 
     sut.on_barcode("99999")
 
@@ -30,7 +30,7 @@ def test_item_not_found():
 
 def test_another_item_not_found():
     display = Display()
-    sut = PointOfSale(display, {"12345": "9.50"})
+    sut = PointOfSale(display, {"12345": 9.50})
 
     sut.on_barcode("76543")
 
